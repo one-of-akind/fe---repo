b88c80a831f72344cfd43337410134a64b0ab3c6
24127358 <dlgiao2437@clc.fitus.edu.vn>
2025-10-22 16:33:00 +0700

    Add files via upload

---
+++ template/script.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var resIcon = L.icon({
+        iconUrl: 'res-icon.jpg',
+        iconSize:     [36, 36]
+    });
+
+    const map = L.map('map').setView([10.7769, 106.7009], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+    const mockRestaurant = {
+        id: 1,
+        name: "Phở Fake HCM",
+        address: "256 P. Fake Lỏ, Bla, Bla, Bla",
+        description: "Một quán phở fake vcl.",
+        latitude: 10.7769,
+        longitude: 106.7009
+    };
+
+    const restaurantMarker = L.marker([mockRestaurant.latitude, mockRestaurant.longitude], 
+                                {icon:resIcon}).addTo(map);
+
+    const detailsPanel = document.getElementById('restaurant-details');
+    const resName = document.getElementById('res-name');
+    const resAddress = document.getElementById('res-address');
+    const resDescription = document.getElementById('res-description');
+    const closeBtn = document.getElementById('close-btn');
+
+    restaurantMarker.on('click', function () {
+        resName.textContent = mockRestaurant.name;
+        resAddress.textContent = "Địa chỉ: " + mockRestaurant.address;
+        resDescription.textContent = mockRestaurant.description;
+        detailsPanel.classList.remove('hidden');
+    });
+    
+    closeBtn.addEventListener('click', function() {
+        detailsPanel.classList.add('hidden');
+    });
+
+    if ('geolocation' in navigator) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            map.setView([position.coords.latitude, position.coords.longitude], 15);
+            L.marker([position.coords.latitude, position.coords.longitude]).addTo(map)
+                .bindPopup('<b>Bạn đang ở đây!</b>').openPopup();
+        });
+    }
+});
